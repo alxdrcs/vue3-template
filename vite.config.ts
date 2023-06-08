263294a42dbfa0d85e56e3c732a3cca13603eb6a
alxdrcs <alxdrcs@126.com>
2023-06-08 15:41:31 +0800

    ci(vite.config): vite基础设置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  plugins: [vue()],
+  server: {
+    host: true,
+    port: 8001,
+    open: true,
+    https: false,
+    proxy: {}
+  }
 })
